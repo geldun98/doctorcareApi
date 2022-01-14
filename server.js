06c49064c29a4d5203460959960b6e96241b123a
geldun98 <geldun98@gmail.com>
2022-01-15 02:34:55 +0700

    update full

--- server.js
+++ server.js
@@ -1,10 +1,88 @@
+const fs = require('fs');
+const bodyParser = require('body-parser');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const server = jsonServer.create();
+const router = jsonServer.router('./database.json');
+const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'));
 
-server.use(middlewares);
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(jsonServer.defaults());
+
+// Check if the user exists in database
+function isAuthenticated({ username, password }) {
+  return userdb.users.findIndex((user) => user.username === username && user.password === password) !== -1;
+}
+function getData(username) {
+  return userdb.users.find((user) => user.username === username);
+}
+
+// Register New User
+server.post('/auth/register', (req, res) => {
+  console.log('register endpoint called; request body:');
+  console.log(req.body);
+  const { username, password, role, exp, major, work } = req.body;
+
+  if (isAuthenticated({ username, password }) === true) {
+    const status = 401;
+    const message = 'Username already exist';
+    res.status(status).json({ status, message });
+    return;
+  }
+
+  fs.readFile('./users.json', (err, data) => {
+    if (err) {
+      const status = 401;
+      const message = err;
+      res.status(status).json({ status, message });
+      return;
+    }
+
+    // Get current users data
+    var data = JSON.parse(data.toString());
+
+    // Get the id of last user
+    var last_item_id = data.users[data.users.length - 1].id;
+
+    //Add new user
+    data.users.push({
+      id: last_item_id + 1,
+      username: username,
+      password: password,
+      role: role,
+      exp: exp,
+      work: work,
+      major: major,
+    }); //add some data
+    var writeData = fs.writeFile('./users.json', JSON.stringify(data), (err, result) => {
+      // WRITE
+      if (err) {
+        const status = 401;
+        const message = err;
+        res.status(status).json({ status, message });
+        return;
+      } else {
+        res.status(200).json('sucess register');
+      }
+    });
+  });
+});
+
+// Login to one of the users from ./users.json
+server.post('/auth/login', (req, res) => {
+  console.log('login endpoint called; request body:');
+  console.log(req.body);
+  const { username, password } = req.body;
+  if (isAuthenticated({ username, password }) === false) {
+    const status = 401;
+    const message = 'Incorrect username or password';
+    res.status(status).json({ status, message });
+    return;
+  }
+
+  res.status(200).json({ user: getData(username) });
+});
+
 server.use(router);
-
 server.listen(port);
